d19b65baf6c670c12747e6333e7372ae0b013457
stepbystep <stepbystepto@163.com>
2016-06-23 17:00:10 +0800

    update ex30.py

--- pythonCodeofYao/ex30.py
+++ pythonCodeofYao/ex30.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 
+people =40
+cars =30
+buses =15
+
+if(people <cars):
+    print "we should take cars"
+elif (people >cars):
+    print "we should not take cars"
+else:
+    print "I can not decide"
+
+if (cars <buses):
+    print "we should take buses"
+elif (cars >buses):
+    print "we should take cars "
+else:
+    print  "we have not a dicision"
+
+if people > buses :
+    print "we should take buses"
+else:
+    print "we should stay at home"
