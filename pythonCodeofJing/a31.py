2aa903d8bf91531ebe298b3944b354be2ed00df5
liunian <liunianyaliunian@163.com>
2016-06-23 23:02:03 +0800

    happy game

---
+++ pythonCodeofJing/a31.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+print "you enter a dark room with two door.do you through door #1 or door #2?"
+
+door = raw_input(">")
+
+if door == "1":
+	print "You hate boss is counting money .what do you do?"
+	print "1.take the many."
+	print "2.scream at the boss."
+
+	boss = raw_input(">")
+
+	if boss == "1":
+		print "Hate boss hugged his money,You have two options to choose # 1 or # 2 . what do you do?"
+
+		hit = raw_input(">")
+
+		if hit == "1":
+			print "Interrupt hand grabbed his money."
+		elif hit == "2":
+			print "You kidnapped your boss he his treasure for life has told you, you move the empty his treasure, filthy rich! "
+		else:
+			print "Suddenly found that the boss is your father! Speechless... "
+
+elif door == "2":
+	print "You have been selected, the one you like play just what kind of the bride? "
+	print "1.Wear like clothing ."
+	print "2.Wearing a cheongsam."
+
+	bride = raw_input(">")
+
+
+	if bride == "1" or bride == "2":
+		print " Xi shi, dressed in a red xi clothing in bridal chamber, etc. That you, congratulations! "
+	elif bride:
+		print "Also can be the old lady wearing cheongsam,don't cry!"
+	else:
+		print "The bride was in fact a man, like you, don't call. Oh god! "
+
+
+
+
+
