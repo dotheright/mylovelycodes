556e99e50b965e0133d5c9611e35839e3c1a95f6
stepbystep <stepbystepto@163.com>
2016-06-23 18:27:01 +0800

    learn the  for list and range

--- pythonCodeofYao/ex32.py
+++ pythonCodeofYao/ex32.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
 
+num = [0,1,2,4,5]
+fruit =["apple","orange","banana"]
+mix = [1,"people",2,"animals",3,"plant"]
+
+for i in num:
+    print "num is %i"%i
+
+for i in fruit:
+    print "fruit is %s"%i
+
+for  i in  mix:
+    print "mix is  %r"%i
+
+element =[]
+
+for  i  in range(6):
+    element.append(i)
+
+for i  in element:
+    print  "element is  %d"%i
