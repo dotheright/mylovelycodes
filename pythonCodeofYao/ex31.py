ae36a473cdc2721cda58d8dc449cdeb3aa69cecf
stepbystep <stepbystepto@163.com>
2016-06-23 17:57:46 +0800

    the test of if in if

--- pythonCodeofYao/ex31.py
+++ pythonCodeofYao/ex31.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
 
+print  "when you graduate from high school ,where do you want do go \n 1: college 2:work"
+
+inch =raw_input();
+if inch == "1":
+    print "so  you dicided to go to school ,and  what specialty do you want ?"
+    choose=raw_input("1:computer ,2:other")
+    if(choose ==  "1"):
+        print "ok,you will become a golden collar workers"
+    else:
+        print "ok,you wiil become a white collar workers"
+else:
+    print "you will become a  blue collar workers"
+
+print "and then ,it  depends you hard work ,After that !"
