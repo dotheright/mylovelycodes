d181da0e15d462b72a5c75896fe8ce633d27d6a6
do the right <stepbystepto@163.com>
2016-11-04 00:34:42 +0800

    big sum

--- theC/poj/1001.c
+++ theC/poj/1001.c
@@ -10,14 +10,21 @@
 #include <stdlib.h>
 #include <assert.h>
 
+void swap(char *a ,char *b)
+{
+	*a=(*a) ^ (*b);
+	*b=(*a) ^ (*b);
+	*a=(*a) ^ (*b);
+}
 /* 先实现一种一位一位的加法,然后再做优化 */
 char * bigNumAdd(char *pcNum1 ,char *pcNum2)
 {
 	unsigned int uiLen1 = 0;
 	unsigned int uiLen2 = 0;
 	unsigned int uiMaxLen = 0;
-	unsigned int uiMinLen = 0;
     int  i  = 0 ;
+	int  j  = 0 ;
+	int  k  = 0 ;
     char *pcMallocBuf = NULL ;
     int  iCarryOn = 0;
 
@@ -34,16 +41,7 @@ char * bigNumAdd(char *pcNum1 ,char *pcNum2)
 		return NULL;
 	}
 
-    if( uiLen1 > uiLen2)
-	{
-		uiMaxLen = uiLen1;
-		uiMinLen = uiLen2;
-	}
-	else
-	{
-		uiMaxLen = uiLen2;
-		uiMinLen = uiLen1;
-	}
+    uiMaxLen = uiLen1 >uiLen2 ? uiLen1:uiLen2;
 	/* 高效的算法最后这里先用一个较大的缓存,其中一个数大于这个缓存的时候再申请 */
 	/* uiMaxLen 只是长度,+'\0'+'进位' 所以这里要 +1+1  */
     pcMallocBuf =(char *)malloc( (uiMaxLen+1+1)*sizeof(char) );
@@ -55,7 +53,32 @@ char * bigNumAdd(char *pcNum1 ,char *pcNum2)
 	memset(pcMallocBuf ,0, (uiMaxLen+2)*sizeof(char));
 
 	//for (i =0 ; i < uiMaxLen+1;)
+	k = 0;
+    for( i = uiLen1-1 , j =uiLen2-1 ; i >=0 && j >= 0 ; j--,i--)
+	{
+		pcMallocBuf[k] = pcNum1[i]-'0'+pcNum2[i]-'0'+'0' + iCarryOn ;
+        iCarryOn = (pcMallocBuf[k]-'0')/10 ;
+		k++;
+	}
+    
+	pcMallocBuf[k] =iCarryOn+'0';
+    if (0 == iCarryOn)
+	{
+		for(i = 0 ; i <k/2;i++ )
+		{
+			swap(&pcMallocBuf[i],&pcMallocBuf[k-i]);
+		}
+	}
+	else
+	{
+		pcMallocBuf[k] = iCarryOn+'0';
+		for(i = 0 ; i<(k+1)/2 ;i++)
+		{
+			swap(&pcMallocBuf[i],&pcMallocBuf[k+1-i]);
+		}
+	}
 
+	return pcMallocBuf;
 
 }
 
