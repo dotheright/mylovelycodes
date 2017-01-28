8bf91b22e5dd6c2fb54de1077253710da6c4b49a
ting PC <stepbystepto@163.com>
2017-01-28 17:34:28 +0800

    multi_print and begin_end

---
+++ ruby/begin_end.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+=begin
+ puts  "this is a comment"
+=end
+ puts "This is main Ruby Program"
+
+END {
+    puts "Terminating Ruby Program"
+ }
+BEGIN {
+     puts "Initializing Ruby Program"
+     # This is a comment
+ }
