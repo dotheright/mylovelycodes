26494f2abefd8e4fc7e1aa1a2d77e8fa99d8216f
dotheright <stepbystepto@163.com>
2017-09-23 13:09:02 +0800

    python web perliminary

---
+++ python/python_web/django/django_site/app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def welcom(request):
+    return HttpResponse("<h1>Welcome to my tiny twitter !</h1>")
