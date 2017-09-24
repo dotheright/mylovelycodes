87ef76ca2ad806f5dc0816d16bc206812308ae5a
dotheright <stepbystepto@163.com>
2017-09-24 11:05:02 +0800

    with bug django site

--- python/python_web/django/django_site/app/models.py
+++ python/python_web/django/django_site/app/models.py
@@ -1,3 +1,17 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 
+KIND_CHOICES =(
+    ('python','python'),
+    ('English','English'),
+    ('Other','Other'),
+)
+
 # Create your models here.
+
+class Moment(models.Model):
+    content =models.CharField(max_length=256)
+    user_name =models.CharField(max_length=32,default="anonymity")
+    kind = models.CharField(max_length=32,choice =KIND_CHOICES,
+                            defaut=KIND_CHOICES[0]);
--- python/python_web/django/django_site/app/views.py
+++ python/python_web/django/django_site/app/views.py
@@ -1,7 +1,25 @@
+#!/usr/bin/env python
+# -*- coding :utf-8 -*-
+
+import os
+
+from app.forms import MomentForm
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
 
 def welcom(request):
-    return HttpResponse("<h1>Welcome to my tiny space !</h1>")
+    if request.method =="POST":
+        form =MomentForm(request.POST)
+        if  form.is_valid() :
+            moment =form.save()
+            moment.save()
+            return HttpResponseRedirect(reverse("app.view.welcome"))
+    else:
+        form =MomentForm()
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print PROJECT_ROOT
+    return render(request, os.path.join(PROJECT_ROOT, 'app/templates', 'moments_input.html'), {'form': form})  
