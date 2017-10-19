731df8040617cd2b22a1d264df1f7795c4be2d12
stepbystep <stepbystepto@163.com>
2017-10-19 16:48:43 +0800

    solve install python error

---
+++ SAMPLE/send.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+url=http://$1:6188/ws/v1/timeline/metrics
+while [ 1 ]
+do
+millon_time=$(( $(date +%s%N) / 1000000 ))
+random=$(( $(date +%s) % 1000 ))
+json="{
+ \"metrics\": [
+ {
+ \"metricname\": \"$2\",
+ \"appid\": \"$3\",
+ \"hostname\": \"$1\",
+ \"timestamp\": ${millon_time},
+ \"starttime\": ${millon_time},
+ \"metrics\": {
+ \"${millon_time}\": ${random}
+ }
+ }
+ ]
+}"
+  
+echo $json 
+curl -i -X POST -H "Content-Type: application/json" -d "${json}" ${url}
+sleep 5
+done
+# ./send.sh master test1 sample
+# cd /var/lib/ambari-server/resources/stacks/HDP/2.3/services/SAMPLE
